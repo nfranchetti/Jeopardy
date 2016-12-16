64e5ff88b8bac6ed2615c1bffe7e28076e18e262
nfranchetti <nick.franchetti@gmail.com>
2016-12-15 22:31:08 -0600

    Lots of word clouds

--- part3/word_cloud.py
+++ part3/word_cloud.py
@@ -8,28 +8,38 @@ import random
 
 from wordcloud import WordCloud, STOPWORDS
 
+img_list = ['j.jpg', 'e.jpg', 'o.jpg', 'p.jpg', 'a.png', 'r.jpg', 'd.jpg', 'y.jpg']
+cluster_list = [1, 2, 3, 4, 24, 6, 7, 8]
 
-def grey_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
-d = path.dirname(__file__)
 
-mask = np.array(Image.open(path.join(d, "trebek.jpg")))
+# def grey_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+#     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
-text = open("answers.txt").read()
 
-stopwords = set(STOPWORDS)
+def make_jeopardy_clouds(img, cluster):
+	d = path.dirname(__file__)
 
-wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
-               random_state=1).generate(text)
+	mask = np.array(Image.open(path.join(d, "images/%s" % img)))
 
-default_colors = wc.to_array()
-plt.title("Custom colors")
-plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3))
-wc.to_file("word_cloud.png")
-plt.axis("off")
-plt.figure()
-plt.title("Default colors")
-plt.imshow(default_colors)
-plt.axis("off")
-plt.show()
+	text = open("clusters/cluster%s.txt" % cluster).read()
+
+	stopwords = set(STOPWORDS)
+
+	wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
+	               random_state=1).generate(text)
+
+	default_colors = wc.to_array()
+	plt.title("Custom colors")
+	plt.imshow(wc.recolor(random_state=3))
+	wc.to_file("word_cloud.png")
+	plt.axis("off")
+	plt.figure()
+	plt.title("Default colors")
+	plt.imshow(default_colors)
+	plt.axis("off")
+	plt.show()
+	wc.to_file("images/final/%s.png" % img[0])
+
+for i in range(len(img_list)):
+	make_jeopardy_clouds(img_list[i], cluster_list[i])
